fa8000d1cd2da8fdbc8c1ffde673deb5b013c75c
w3villa-anuj-pal <anuj.pal@w3villa.com>
2022-12-20 12:45:37 +0530

    add new ruby practice file

---
+++ practice_test.rb
@@ -0,0 +1,14 @@
+# x = "Hello"
+# y = x.empty? ? 1 : 2
+# p y
+# p %i(hello anuj pal)
+# p %q(hello anuj pal)
+# p %w(hello anuj pal)
+# p %s(hello anuj pal)
+
+# x = 0
+# 5.times do 
+#   x+=1
+# end
+
+# puts x
---
+++ times.rb
@@ -0,0 +1,19 @@
+# x = 0 
+# 5.times do
+#   x+=1
+# end
+# puts x 
+
+
+x = 0 
+5.times do |i|
+  x+=i
+  puts x
+end
+puts x 
+
+puts
+
+# 5.times do |i|
+#   puts i
+# end
